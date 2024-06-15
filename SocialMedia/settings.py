0624ae347a1e04892490f0dd6f1b7f65b789aa04
kajal yadav <kajalyad31072001@gmail.com>
2024-06-15 23:03:22 +0530

    fixed typo

--- SocialMedia/settings.py
+++ SocialMedia/settings.py
@@ -131,9 +131,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.UserRateThrottle',
-    ],
     'DEFAULT_THROTTLE_RATES': {
         'friend_request': '3/min',
     },
